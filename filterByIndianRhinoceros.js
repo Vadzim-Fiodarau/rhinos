ea881c20dcaf4dfa60a75cf4205edfb255a97c6d
Vadzim-Fiodarau <86311129+Vadzim-Fiodarau@users.noreply.github.com>
2023-03-08 23:09:39 -0600

    Update filterByIndianRhinoceros.js

--- filterByIndianRhinoceros.js
+++ filterByIndianRhinoceros.js
@@ -1,19 +1,11 @@
+import { onFilterRhino } from '../../support/page_objects/filterRhino'
+
 describe('Verify that when filtering by "indian_rhinoceros" in the GET action, the newly created rhino is NOT present in the search result', ()=> {
+    beforeEach('open application', () => {
+        cy.openPageHome()
+    })
 
-    it.only('Verify filte by "indian_rhinoceros" ', ()=>{
-        cy.visit('/')
-
-        cy.get('.rhino-inputs form').then(form => {
-            cy.wrap(form).eq(1).find('[id="create_rhino_species"]').type('indian_rhinoceros')
-            cy.wrap(form).eq(1).find('button').click()
-
-
-        })
-
-        cy.get('table').then( table => {
-           expect(cy.wrap(table)).not.to.equal('javan_rhinoceros')
-           expect(cy.wrap(table)).not.to.equal('Clyde500')
-        })
-
+    it('Verify filter by "indian_rhinoceros" ', ()=>{
+        onFilterRhino.filterRhinoByIndianRhinoceres('indian_rhinocero')
     })
 })
