aaa4a9c09300d9fe7ddeec80992ff4cf6aa26bdc
Vadzim-Fiodarau <86311129+Vadzim-Fiodarau@users.noreply.github.com>
2023-03-08 23:09:00 -0600

    Update createNewRhino.js

--- createNewRhino.js
+++ createNewRhino.js
@@ -1,24 +1,11 @@
-describe('Create a new rhino with the name "Clyde5000" and the species "javan_rhinoceros"', ()=> {
-    
+import { onCreateNewRhino } from './../../support/page_objects/createRhino'
+
+describe('Create a new rhino with the name "Clyde5000" and the species "javan_rhinoceros"', () => {
+    beforeEach('open application', () => {
+        cy.openPageHome()
+    })
+
     it('Create a new rhino', () => {
-        cy.visit('/')
-
-        cy.intercept('POST', 'http://localhost:4000/rhinoceros').as('createRhionos')
-
-        cy.get('.rhino-inputs form').then(form => {
-            cy.wrap(form).first().find('[id="create_rhino_name"]').type('Clyde5000')
-            cy.wrap(form).first().find('[id="create_rhino_species"]').type('javan_rhinoceros')
-            cy.wrap(form).first().find('button').click()
-
-        })
-
-        cy.wait('@createRhionos').then(xhr => {
-            console.log(xhr)
-            expect(xhr.response.statusCode).to.equal(200)
-            expect(xhr.request.body.name).to.equal('Clyde5000')
-            expect(xhr.request.body.species).to.equal('javan_rhinoceros')
-            expect(xhr.response.body.name).to.equal('Clyde5000')
-            expect(xhr.response.body.species).to.equal('javan_rhinoceros')
-        })
+        onCreateNewRhino.createRhino('Clyde5000', 'javan_rhinoceros')
     })
 })
