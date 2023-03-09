af9d9860e6c0fa771562dcb16c4ed91224283079
Vadzim-Fiodarau <86311129+Vadzim-Fiodarau@users.noreply.github.com>
2023-03-08 23:11:35 -0600

    Update rhinoAppearInTable.js

--- rhinoAppearInTable.js
+++ rhinoAppearInTable.js
@@ -1,29 +1,11 @@
+import { onRhinoDisplaed } from './../../support/page_objects/rhinoDisplayedInTable'
+
 describe('Make sure that newly just created Rhino displayed in table', ()=>{
+    beforeEach('open application', () => {
+        cy.openPageHome()
+    })
 
     it('Test fail', () => {
-
-        cy.visit('/')
-        cy.intercept('POST', 'http://localhost:4000/rhinoceros').as('createRhionos')
-
-        cy.get('.rhino-inputs form').then(form => {
-            cy.wrap(form).first().find('[id="create_rhino_name"]').type('Clyde5000')
-            cy.wrap(form).first().find('[id="create_rhino_species"]').type('javan_rhinoceros')
-            cy.wrap(form).first().find('button').click()
-
-        })
-
-        
-
-        cy.wait('@createRhionos').then(xhr => {
-            console.log(xhr)
-            expect(xhr.response.statusCode).to.equal(200)
-            expect(xhr.request.body.name).to.equal('Clyde5000')
-            expect(xhr.request.body.species).to.equal('javan_rhinoceros')
-            expect(xhr.response.body.name).to.equal('Clyde5000')
-            expect(xhr.response.body.species).to.equal('javan_rhinoceros')
-        })
-
-        cy.get('table').should('contain', 'javan_rhinoceros')
-
+        onRhinoDisplaed.rhinoDisplayed('Clyde500', 'javan_rhinoceros')
     })
 })
