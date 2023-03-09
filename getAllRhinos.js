d9b244abff668ae395171af1e0966b9d9c67af29
Vadzim-Fiodarau <86311129+Vadzim-Fiodarau@users.noreply.github.com>
2023-03-08 23:11:01 -0600

    Update getAllRhinos.js

--- getAllRhinos.js
+++ getAllRhinos.js
@@ -1,24 +1,13 @@
+import { onGetAllRhinos } from '../../support/page_objects/getAllRhinos'
+
 describe('Make sure that filter without any value get all rhinos from system', () => {
 
+    beforeEach('open application', () => {
+        cy.openPageHome()
+    })
+
     it('Get all rhinos without value', () => {
-
-        cy.visit('/')
-
-        cy.intercept('GET', 'http://localhost:4000/rhinoceros?').as('getRhinos')
-
-        cy.get('.rhino-inputs form').then(form => {
-            cy.wrap(form).eq(1).find('input').should('contain', '')
-            cy.wrap(form).eq(1).find('button').click()
-
-
-        })
-
-        cy.wait('@getRhinos').then(xhr => {
-            const rhinosAmount = xhr.response.body.rhinoceroses.length
-            expect(xhr.response.statusCode).to.equal(200)
-            expect(xhr.response.body.rhinoceroses.length).to.equal(rhinosAmount)
-
-        })
+        onGetAllRhinos.getRhinosWithEmptyValue()
 
     })
 })
